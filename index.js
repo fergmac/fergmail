e7aea67ee287661b086677e40a262debf51bbe3e
fergmac <f.macconnell@gmail.com>
2017-09-13 12:12:18 -0700

    refactor server into individual files

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // common js modules on the server side due to no support for ess2015 import in node
 const express = require('express');
+require('./services/passport');
 
 // set up configuration to listen for incoming requests routed to express from node and route to different handlers
 const app = express(); 
-// route handler
-app.get('/', (req, res) => {
-    res.send({ hello: 'ferg' });
-});
+
+// require returns a function and we immediately call that function with app
+require('./routes/authRoutes')(app);
+
 // in dev use 5000 in production use whatever port heroku is telling us to
 const PORT = process.env.PORT || 5000;
 
---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+const passport = require('passport');
+
+module.exports = app => {
+// route handler: attempt to auth user coming in by this route and use google oauth
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+})
+);
+
+// 'google' identifier for googleStrategy, user visits this route with 'code'
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+};
