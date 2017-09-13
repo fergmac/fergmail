62f6b423f26549cb9814848f0d9100e3c3c3dd27
fergmac <f.macconnell@gmail.com>
2017-09-13 12:58:01 -0700

    add mongoose and connect to mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // common js modules on the server side due to no support for ess2015 import in node
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 // set up configuration to listen for incoming requests routed to express from node and route to different handlers
 const app = express(); 
