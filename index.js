cc4115cf50643f4e888e060d03e3d93a309e033b
fergmac <f.macconnell@gmail.com>
2017-09-12 20:55:53 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// common js modules on the server side due to no support for ess2015 import in node
+const express = require('express');
+
+// set up configuration to listen for incoming requests routed to express from node and route to different handlers
+const app = express(); 
+// route handler
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+// in dev use 5000 in production use whatever port heroku is telling us to
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
