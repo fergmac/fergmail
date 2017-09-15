e749b48d88b7faff31c7384469695115aa1ee476
fergmac <f.macconnell@gmail.com>
2017-09-15 08:52:53 -0700

    create fetchUser action and add second proxy for /api/*

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 
 import App from './components/App';
@@ -10,7 +11,7 @@ import reducers from './reducers';
 
 
 // create new instance of redux store
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>, 
