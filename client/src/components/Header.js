979621f85007ce849c11283edc2930960bc0281e
fergmac <f.macconnell@gmail.com>
2017-09-15 08:32:29 -0700

    add materialize-css and update Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,18 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>Header</div>
+            <nav>
+                <div className="nav-wrapper">
+                    <a className="left brand-logo">
+                        FergMail
+                    </a>
+                    <ul className="right">
+                        <li>
+                            <a>Login With Google</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+
 import App from './components/App';
 import reducers from './reducers';
+
 
 // create new instance of redux store
 const store = createStore(reducers, {}, applyMiddleware());
