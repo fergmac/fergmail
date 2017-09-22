adbf1862f436e7a435f07579ee81021af7d12a01
fergmac <f.macconnell@gmail.com>
2017-09-22 14:30:15 -0700

    add body-parser and billingRoute stripe backend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 // declare model first to avoid schema not registered  error
 require('./models/User');
@@ -13,6 +14,9 @@ mongoose.connect(keys.mongoURI);
 
 // set up configuration to listen for incoming requests routed to express from node and route to different handlers
 const app = express(); 
+
+// incoming request payload set to req.body
+app.use(bodyParser.json());
 
 app.use(
     cookieSession({
@@ -25,6 +29,8 @@ app.use(passport.session());
 
 // require returns a function and we immediately call that function with app
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
 
 // in dev use 5000 in production use whatever port heroku is telling us to
 const PORT = process.env.PORT || 5000;
