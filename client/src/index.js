70b7ba828dca7e55dce73db58d5a05b37e828d80
fergmac <f.macconnell@gmail.com>
2017-09-26 09:34:21 -0700

    add axios for testing emails

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-
-
 import App from './components/App';
 import reducers from './reducers';
-
+// to test emails
+// import axios from 'axios';
+// window.axios = axios;
 
 // create new instance of redux store
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
